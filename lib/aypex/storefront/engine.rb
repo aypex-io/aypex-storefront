8c03ef8e94f855c19c357490248e86804815fd47
Matthew Kennedy <m.kennedy@me.com>
2023-01-04 16:12:34 +0000

    Add detectors

--- lib/aypex/storefront/engine.rb
+++ lib/aypex/storefront/engine.rb
@@ -6,6 +6,30 @@ module Aypex
       isolate_namespace Aypex
       engine_name "aypex_storefront"
 
+      def self.admin_available?
+        @admin_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?('Aypex::Admin::Engine')
+      end
+
+      def self.api_available?
+        @api_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?('Aypex::Api::Engine')
+      end
+
+      def self.checkout_available?
+        @checkout_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?('Aypex::Checkout::Engine')
+      end
+
+      def self.cli_available?
+        @cli_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?('Aypex::Cli::Engine')
+      end
+
+      def self.emails_available?
+        @emails_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?('Aypex::Emails::Engine')
+      end
+
+      def self.sample_available?
+        @sample_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?('AypexSample::Engine')
+      end
+
       config.middleware.use 'Aypex::Storefront::Middleware::SeoAssist'
 
       # Prevent XSS but allow text formatting
